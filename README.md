56aa84469f5c83698dfdab7f2a13295259c06c79
Brian Emil Hartz <brianhartz@gmail.com>
2014-04-10 14:28:58 -0600

    updated readme, and cleaned up main.js

--- README.md
+++ README.md
@@ -4,8 +4,9 @@ js6-hide-a-form
 
 First iteration- submit button
 
-http://jsfiddle.net/Bux9r/
+http://jsfiddle.net/hartzis/APBjL/
 
 
 Second iteration- live update
 
+http://jsfiddle.net/hartzis/J6b9D/
--- js/main.js
+++ js/main.js
@@ -8,25 +8,12 @@ $(document).on('ready', function() {
         $('#user-input-form').slideToggle("slow");
     })
 
-    // SUBMIT BUTTON
-    // $('#user-input-form input[type="submit"]').on('click', function() {
-    //     copyFormToInfo();
-    //     return false;
-    // })
-
     var copyInfoToForm = function() {
         $('#form-name').val($('#user-name').text());
         $('#form-bio').val($('#user-bio').text());
         $('#form-books').val($('#user-books').text());
         $('#form-javascript').val($('#user-javascript').text());
     }
-
-    // var copyFormToInfo = function() {
-    //     $('#user-name').text($('#form-name').val());
-    //     $('#user-bio').text($('#form-bio').val());
-    //     $('#user-books').text($('#form-books').val());
-    //     $('#user-javascript').text($('#form-javascript').val());
-    // }
 
     // LISTENER FOR ALL INPUT
     $('.form-input').keyup(function() {
