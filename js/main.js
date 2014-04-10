25d65fc7341f9bcb0d88d7ebf44ce6f069f5144c
Brian Emil Hartz <brianhartz@gmail.com>
2014-04-10 11:56:32 -0600

    submit button works to change info

--- js/main.js
+++ js/main.js
@@ -4,13 +4,28 @@ $(document).on('ready', function() {
     $('#user-input-form').hide();
     //toggle show/hide form
     $('#collapse-form').on('click', function() {
-        $('#user-input-form').toggle("slow");
+        $('#user-input-form').slideToggle("slow");
+        copyInfoToForm();
     })
 
+    $('#user-input-form input[type="submit"]').on('click', function() {
+        copyFormToInfo();
+        return false;
+    })
 
+    var copyInfoToForm = function() {
+        $('#form-name').val($('#user-name').text());
+        $('#form-bio').val($('#user-bio').text());
+        $('#form-books').val($('#user-books').text());
+        $('#form-javascript').val($('#user-javascript').text());
+    }
 
-
-
+    var copyFormToInfo = function() {
+        $('#user-name').text($('#form-name').val());
+        $('#user-bio').text($('#form-bio').val());
+        $('#user-books').text($('#form-books').val());
+        $('#user-javascript').text($('#form-javascript').val());
+    }
 
 
 });
