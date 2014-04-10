f9211fa8a69ea54ffd8c0b3f25e002f4d24ca069
Brian Emil Hartz <brianhartz@gmail.com>
2014-04-09 20:00:57 -0600

    add more layout and form. also added toggle form hide/show

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
 $(document).on('ready', function() {
-  
+
+    //initially hide form
+    $('#user-input-form').hide();
+    //toggle show/hide form
+    $('#collapse-form').on('click', function() {
+        $('#user-input-form').toggle("slow");
+    })
+
+
+
+
+
+
+
 });
